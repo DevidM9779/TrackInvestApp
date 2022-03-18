42f53f32d5547da1d6bf2fd689cb40bac3f7c28e
David Milan-Medina <ddavidmilan@hotmail.es>
2022-03-17 19:35:12 -0500

    Initial Commit

---
+++ TrackInvest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackInvest
+//
+//  Created by David Milan-Medina on 3/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrackInvest/TrackInvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackInvestApp.swift
+//  TrackInvest
+//
+//  Created by David Milan-Medina on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackInvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
